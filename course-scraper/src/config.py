085bc2371c1f6db3f08522872ac8bd066b9764a7
TobeTek <katchyemma@gmail.com>
2023-09-23 19:17:10 +0100

    initial implementation Google SkillBoost scraper

---
+++ course-scraper/src/config.py
@@ -0,0 +1,17 @@
+class CONFIG:
+    CHROME_USER_DATA_DIR = r"C:\Users\user\AppData\Local\Google\Chrome\User Data"
+    CHROME_USER = "Default"
+
+    # XXX: Fill this
+    GCB_EMAIL = ""
+    GCB_PASSWORD = ""
+
+    GCB_LOGIN_URL = "https://www.cloudskillsboost.google/users/sign_in"
+
+    GCB_HOME_URL = "https://www.cloudskillsboost.google/"
+    
+    DATA_PATH = r"C:\Users\user\Documents\coding\learning-path-index\new-data"
+    JOURNEY_CODE = "CLMG015"
+    JOURNEY_URL = "https://www.cloudskillsboost.google/journeys/118"
+    # GCB_MACHINE_LEARNING_PATH = "https://www.cloudskillsboost.google/journeys/17"
+
