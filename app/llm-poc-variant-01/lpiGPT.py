613e76e93b8332be1303c978e8084a394d7687ed
Mani Sarkar <sadhak001@gmail.com>
2023-10-15 20:20:01 +0100

    Removed the use of env variables instead using pythons argsparser to capture such parameters as CLI agruments as per code review comments from Wendy

--- app/llm-poc-variant-01/lpiGPT.py
+++ app/llm-poc-variant-01/lpiGPT.py
@@ -4,19 +4,9 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import Ollama
-import os
 import argparse
 import time
 from datetime import datetime
-
-model = os.environ.get("MODEL", "llama2-uncensored")
-# For embeddings model, the example uses a sentence-transformers model
-# https://www.sbert.net/docs/pretrained_models.html 
-# "The all-mpnet-base-v2 model provides the best quality, while all-MiniLM-L6-v2 is 5 times faster 
-# and still offers good quality."
-embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME", "all-MiniLM-L6-v2")
-persist_directory = os.environ.get("PERSIST_DIRECTORY", "vector_db")
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
 from constants import CHROMA_SETTINGS
 
@@ -25,15 +15,20 @@ def main():
 
     # Parse the command line arguments
     args = parse_arguments()
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    vector_db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-    retriever = vector_db.as_retriever(search_kwargs={"k": target_source_chunks})
+    embeddings = HuggingFaceEmbeddings(model_name=args.embeddings_model_name)
+    vector_db = Chroma(
+        persist_directory=args.persist_directory, 
+        embedding_function=embeddings, 
+        client_settings=CHROMA_SETTINGS
+    )
+    retriever = vector_db.as_retriever(search_kwargs={"k": args.target_source_chunks})
+
     # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
 
-    llm = Ollama(model=model, callbacks=callbacks)
+    llm = Ollama(model=args.chat_model, callbacks=callbacks)
 
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=not args.hide_source)
 
     end = time.time()
 
@@ -69,6 +64,22 @@ def main():
 def parse_arguments():
     parser = argparse.ArgumentParser(description='lpiGPT: Ask questions to your documents without an internet connection, '
                                                  'using the power of LLMs.')
+    # https://huggingface.co/spaces/lmsys/chatbot-arena-leaderboard or https://ollama.ai/library
+    parser.add_argument("--chat-model", "-CM", action='store', default="llama2-uncensored",
+                        help='Use this flag to set the InstructGPT or Chat model name, see https://huggingface.co/spaces/lmsys/chatbot-arena-leaderboard or https://ollama.ai/library for more names.')
+    # For embeddings model, the example uses a sentence-transformers model
+    # https://www.sbert.net/docs/pretrained_models.html 
+    # "The all-mpnet-base-v2 model provides the best quality, while all-MiniLM-L6-v2 is 5 times faster 
+    # and still offers good quality."
+    parser.add_argument("--embeddings-model-name", "-EM", action='store', default="all-MiniLM-L6-v2",
+                        help='Use this flag to set the Embeddings model name, see https://www.sbert.net/docs/pretrained_models.html for examples of names. Use the same model as used for ingesting the documents (ingest.py)')
+
+    parser.add_argument("--persist-directory", "-P", action='store', default="vector_db",
+                        help='Use this flag to specify the name of the vector database i.e. vector_db - this will be a folder on the local machine.')
+
+    parser.add_argument("--target-source-chunks", "-C", action='store', default=500,
+                        help='Use this flag to specify the name chunk size to use to chunk source data.')
+    
     parser.add_argument("--hide-source", "-S", action='store_true',
                         help='Use this flag to disable printing of source documents used for answers.')
 
