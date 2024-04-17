e4554d98cdd37f20988892b204d4284c9ec696f1
TobeTek <katchyemma@gmail.com>
2024-04-17 12:34:47 +0100

    Update Google Skill Boost scraper

--- app/course-scraper/src/scrapers/google_cloud_skill_boost/scrape_course_template.py
+++ app/course-scraper/src/scrapers/google_cloud_skill_boost/scrape_course_template.py
@@ -10,78 +10,88 @@ from lxml import etree
 from scrapers.google_cloud_skill_boost.models import Activity, Course, CourseSubmodule
 from utils import get_safestring
 
-COURSE_CODE = "CLMML11"
 
-DATA_FOLDER = Path(CONFIG.DATA_PATH, COURSE_CODE)
+PARENT_DATA_FOLDER = Path(CONFIG.DATA_PATH)
+DATA_FOLDER = Path(CONFIG.DATA_PATH, 'course_data')
 DATA_FOLDER.mkdir(exist_ok=True, parents=True)
 
-course_modules_mapping = {}
-with open(DATA_FOLDER.joinpath(f"{COURSE_CODE}-Courses.csv")) as f:
-    course_meta = io.StringIO(f.read())
-
-csvreader = csv.DictReader(course_meta)
-for course in csvreader:
-    # TODO: Support scraping GCB Labs
-    if "labs" in course["title"].lower():
-        continue
-    r = requests.get(course["link"])
-    print(str(r.content)[:100])
-    html_parser = etree.HTMLParser()
-    dom = etree.fromstring(r.content, html_parser)
-
-    prerequisites = None
-    if prerequisites := dom.xpath(
-        "(//div[div/text() = 'Prerequisites'])/following-sibling::div/text()"
-    ):
-        prerequisites = "".join(prerequisites[0]).replace("\n", " ")
-
-    if course_modules := dom.xpath("//ql-course/@modules"):
-        course_modules = course_modules[0]
-        course_modules = unescape(course_modules)
-    else:
+for learning_path in PARENT_DATA_FOLDER.iterdir():
+    if not learning_path.is_file() or ".csv" not in learning_path.suffixes:
         continue
 
-    course_modules_mapping[course["title"]] = course_modules
+    course_modules_mapping = {}
+    with open(learning_path) as f:
+        course_meta = io.StringIO(f.read())
 
-    with open(
-        DATA_FOLDER.joinpath(f"{COURSE_CODE}-Modules-Meta.csv"),
-        "a",
-        encoding="utf-8",
-    ) as f:
-        csvwriter = csv.writer(f)
-        print(prerequisites)
-        csvwriter.writerow([course["title"], course["link"], prerequisites])
+    csvreader = csv.DictReader(course_meta)
+    for course in csvreader:
+        # TODO: Support scraping GCB Labs
+        if "labs" in course["title"].lower():
+            continue
+        r = requests.get(course["link"])
+        html_parser = etree.HTMLParser()
+        dom = etree.fromstring(r.content, html_parser)
 
-for course_title, course_module in course_modules_mapping.items():
-    parsed_courses = [c for c in Course.parse_raw(course_module).__root__]
-    submodule_activities: dict[CourseSubmodule, list[dict]] = defaultdict(list)
+        prerequisites = None
+        if prerequisites := dom.xpath(
+            "(//div[div/text() = 'Prerequisites'])/following-sibling::div/text()"
+        ):
+            prerequisites = "".join(prerequisites[0]).replace("\n", " ")
 
-    # Link Submodules with their activities
-    for submodule in parsed_courses:
-        for step in submodule.steps:
-            submodule_activities[submodule].extend(
-                [activity.dict() for activity in step.activities]
-            )
+        if course_modules := dom.xpath("//ql-course/@modules"):
+            course_modules = course_modules[0]
+            course_modules = unescape(course_modules)
+        else:
+            continue
 
-    course_title = get_safestring(course_title)
+        course_modules_mapping[course["title"]] = course_modules
 
-    for submodule in submodule_activities:
-        submodule_title = get_safestring(submodule.title)
         with open(
-            DATA_FOLDER.joinpath(f"{submodule_title}.csv"),
-            "w",
+            DATA_FOLDER.joinpath(f"{learning_path.name}-Modules-Meta.csv"),
+            "a",
             encoding="utf-8",
         ) as f:
-            fieldnames = Activity.__fields__.keys()
+            csvwriter = csv.writer(f)
+            print(prerequisites)
+            csvwriter.writerow([course["title"], course["link"], prerequisites])
+
+    for course_title, course_module in course_modules_mapping.items():
+        parsed_courses = [c for c in Course.parse_raw(course_module).__root__]
+        submodule_activities: dict[CourseSubmodule, list[dict]] = defaultdict(list)
+
+        # Link Submodules with their activities
+        for submodule in parsed_courses:
+            for step in submodule.steps:
+                submodule_activities[submodule].extend(
+                    [activity.dict() for activity in step.activities]
+                )
+
+        course_title = get_safestring(course_title)
+
+        for submodule in submodule_activities:
+            submodule_title = get_safestring(submodule.title)
+            with open(
+                DATA_FOLDER.joinpath(f"{submodule_title}.csv"),
+                "w",
+                encoding="utf-8",
+            ) as f:
+                fieldnames = Activity.__fields__.keys()
+                csvwriter = csv.DictWriter(f, fieldnames)
+                csvwriter.writeheader()
+                csvwriter.writerows(submodule_activities[submodule])
+
+        with open(
+            DATA_FOLDER.joinpath(f"{course_title}.csv"), "w", encoding="utf-8"
+        ) as f:
+            excluded_fields = {"steps", "expanded"}
+            fieldnames = sorted(
+                set(CourseSubmodule.__fields__.keys()) - excluded_fields
+            )
             csvwriter = csv.DictWriter(f, fieldnames)
             csvwriter.writeheader()
-            csvwriter.writerows(submodule_activities[submodule])
-
-    with open(DATA_FOLDER.joinpath(f"{course_title}.csv"), "w", encoding="utf-8") as f:
-        excluded_fields = {"steps", "expanded"}
-        fieldnames = sorted(set(CourseSubmodule.__fields__.keys()) - excluded_fields)
-        csvwriter = csv.DictWriter(f, fieldnames)
-        csvwriter.writeheader()
-        csvwriter.writerows(
-            [submodule.dict(exclude=excluded_fields) for submodule in parsed_courses]
-        )
+            csvwriter.writerows(
+                [
+                    submodule.dict(exclude=excluded_fields)
+                    for submodule in parsed_courses
+                ]
+            )
--- app/course-scraper/src/scrapers/google_cloud_skill_boost/scrape_journey.py
+++ app/course-scraper/src/scrapers/google_cloud_skill_boost/scrape_journey.py
@@ -4,23 +4,26 @@ from urllib.parse import urljoin
 from lxml import etree
 
 from scrapers.google_cloud_skill_boost import pages
+from utils import get_safestring
 from config import CONFIG
 import requests
 
-COURSE_CODE = "CLMML11"
+# COURSE_CODE = "CLMML11"
 GCSB_JOURNEY_URL = "https://www.cloudskillsboost.google/journeys/17"
 GCSB_HOME_URL = "https://www.cloudskillsboost.google/"
 GCSB_LOGIN_URL = "https://www.cloudskillsboost.google/users/sign_in"
 
-DATA_FOLDER = Path(CONFIG.DATA_PATH, COURSE_CODE)
+DATA_FOLDER = Path(CONFIG.DATA_PATH)
 DATA_FOLDER.mkdir(exist_ok=True, parents=True)
 
 
 # Open Journey Path
 def extract_ml_learning_path() -> list[dict]:
-    r = requests.get(GCSB_JOURNEY_URL)
+    r = requests.get(GCSB_JOURNEY_URL, allow_redirects=True)
     html_parser = etree.HTMLParser()
     dom = etree.fromstring(r.content, html_parser)
+
+    course_name = get_safestring(dom.xpath(pages.GCSBLearningJourneyPage.learning_plan_title)[0])
 
     data = []
     for journey in dom.xpath(pages.GCSBLearningJourneyPage.journeys):
@@ -40,14 +43,12 @@ def extract_ml_learning_path() -> list[dict]:
             }
         )
 
-    return data
+    return course_name, data
 
 
-ml_learning_path = extract_ml_learning_path()
+course_name, ml_learning_path = extract_ml_learning_path()
 
-with open(
-    DATA_FOLDER.joinpath(f"{COURSE_CODE}-Courses.csv"), "w", encoding="utf-8"
-) as f:
+with open(DATA_FOLDER.joinpath(f"{course_name}.csv"), "w", encoding="utf-8") as f:
     csvwriter = DictWriter(f, fieldnames=["title", "details", "description", "link"])
     csvwriter.writeheader()
     csvwriter.writerows(ml_learning_path)
