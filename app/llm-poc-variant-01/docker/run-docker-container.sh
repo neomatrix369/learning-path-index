f626e70dba6092cf4f051bef0750083fa2148aa6
Mani Sarkar <sadhak001@gmail.com>
2024-07-27 14:53:41 +0100

    Adapting shellscript to pass the OLLAMA_HOST into the docker container to use it inside the python code - to communicate with the Ollama server outside the docker container

--- app/llm-poc-variant-01/docker/run-docker-container.sh
+++ app/llm-poc-variant-01/docker/run-docker-container.sh
@@ -21,11 +21,13 @@ OLLAMA_VOLUME_SHARED="--volume $(which ollama):/usr/bin/ollama"
 HF_CACHE_SHARED="--volume ${LOCAL_MODEL_FOLDER}/.cache:/root/.cache"
 
 set -x
-pullImage
+# pullImage
 time docker run --rm -it                   \
                 --platform="linux/amd64"   \
                 --network="host"           \
+                --add-host=host.docker.internal:host-gateway \
                 --workdir "${WORKDIR}"     \
+                --env OLLAMA_HOST="http://host.docker.internal:11434" \
                 ${HF_CACHE_SHARED}         \
                 ${MODEL_VOLUME_SHARED}     \
                 ${OLLAMA_VOLUME_SHARED}    \
